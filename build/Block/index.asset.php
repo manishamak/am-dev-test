f12e21aab922f01ec16c8a3538498cb7e1b4f2b9
Manisha Makhija <manishamakhija@yahoo.com>
2023-07-20 20:35:59 +0530

    dependency update

--- build/Block/index.asset.php
+++ build/Block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f07f764e57ee4895290b');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '83a0643f8a36a5fbd405');
--- build/Block/index.js
+++ build/Block/index.js
@@ -1,2 +1,335 @@
-(()=>{"use strict";var e={n:t=>{var a=t&&t.__esModule?()=>t.default:()=>t;return e.d(a,{a}),a},d:(t,a)=>{for(var n in a)e.o(a,n)&&!e.o(t,n)&&Object.defineProperty(t,n,{enumerable:!0,get:a[n]})},o:(e,t)=>Object.prototype.hasOwnProperty.call(e,t)};const t=window.wp.blocks,a=window.wp.element,n=window.wp.components,l=window.wp.apiFetch;var r=e.n(l);const i=window.wp.blockEditor,o=window.wp.i18n;function s(e){let{header:t,attributes:n}=e;const l=new Array;for(let e=0;e<t.length;e++){const r=t[e].replace(" ","_").toLowerCase();l.push([(0,a.createElement)(a.Fragment,null,n[r]&&(0,a.createElement)("th",null,t[e]))])}return l}function m(e){let{rows:t,attributes:n}=e;const l=new Array;return Object.values(t).forEach((e=>{const t=new Date(1e3*e.date);l.push([(0,a.createElement)("tr",{key:"mm-api-data-display-table-rows"},n.id&&(0,a.createElement)("td",null,e.id),n.first_name&&(0,a.createElement)("td",null,e.fname),n.last_name&&(0,a.createElement)("td",null,e.lname),n.email&&(0,a.createElement)("td",null,e.email),n.date&&(0,a.createElement)("td",null,t.toLocaleDateString("en-US")))])})),l}const c=JSON.parse('{"u2":"manisha-makhija/api-data-display"}');(0,t.registerBlockType)(c.u2,{example:{attributes:{preview:!0}},edit:function(e){let{attributes:t,setAttributes:l}=e;const[c,d]=(0,a.useState)(null),[h,u]=(0,a.useState)(null),[p,w]=(0,a.useState)(!1),E=[(0,a.createElement)(i.InspectorControls,{key:"mm-api-data-display-block-inspector-controls"},(0,a.createElement)(n.PanelBody,{title:(0,o.__)("Manage Table columns","manisha-makhija"),key:"mm-api-data-display-block-panel-body",initialOpen:!0},(0,a.createElement)(n.ToggleControl,{label:(0,o.__)("Show ID","manisha-makhija"),onChange:e=>{l({id:e})},checked:t.id}),(0,a.createElement)(n.ToggleControl,{label:(0,o.__)("Show First Name","manisha-makhija"),onChange:e=>{l({first_name:e})},checked:t.first_name}),(0,a.createElement)(n.ToggleControl,{label:(0,o.__)("Show Last Name","manisha-makhija"),onChange:e=>{l({last_name:e})},checked:t.last_name}),(0,a.createElement)(n.ToggleControl,{label:(0,o.__)("Show Email","manisha-makhija"),onChange:e=>{l({email:e})},checked:t.email}),(0,a.createElement)(n.ToggleControl,{label:(0,o.__)("Show Date","manisha-makhija"),onChange:e=>{l({date:e})},checked:t.date})))];if((0,a.useEffect)((()=>{r()({path:mmApiDataDisplayBlock.custom_endpoint}).then((e=>{w(!0),u(e)}),(e=>{w(!0),d(e)}))}),[]),c)return(0,a.createElement)("p",null,(0,o.sprintf)(/* Translators: %s - error message. */
-(0,o.__)("ERROR: %s","manisha-makhija"),c.message)," ");if(!p)return(0,a.createElement)("p",null,(0,o.__)("Loading response…","manisha-makhija")," ");if(h){const e=h.data.headers,n=h.data.rows;if(e.length<0||Object.keys(n).length<0)return(0,a.createElement)("p",null,(0,o.__)("No data found in API","manisha-makhija"));E.push([(0,a.createElement)("div",{key:"manisha-makhija-table-data-wrap",className:"manisha-makhija-table-data-wrap"},(0,a.createElement)("table",{border:"1",style:{width:"100%",textAlign:"center"}},h.title&&(0,a.createElement)("tr",null,(0,a.createElement)("td",{colSpan:"5",style:{fontSize:"xx-large",padding:"25px"}},h.title)),(0,a.createElement)("tr",null,(0,a.createElement)(s,{header:e,attributes:t})),(0,a.createElement)(m,{rows:n,attributes:t})))])}return E},save:()=>null})})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/Block/edit.js":
+/*!***************************!*\
+  !*** ./src/Block/edit.js ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ Edit)
+/* harmony export */ });
+/* harmony import */ var _wordpress_element__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @wordpress/element */ "@wordpress/element");
+/* harmony import */ var _wordpress_element__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_wordpress_element__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _wordpress_components__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @wordpress/components */ "@wordpress/components");
+/* harmony import */ var _wordpress_components__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_wordpress_components__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _wordpress_api_fetch__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @wordpress/api-fetch */ "@wordpress/api-fetch");
+/* harmony import */ var _wordpress_api_fetch__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_wordpress_api_fetch__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _wordpress_block_editor__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @wordpress/block-editor */ "@wordpress/block-editor");
+/* harmony import */ var _wordpress_block_editor__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_wordpress_block_editor__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var _wordpress_i18n__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @wordpress/i18n */ "@wordpress/i18n");
+/* harmony import */ var _wordpress_i18n__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_wordpress_i18n__WEBPACK_IMPORTED_MODULE_4__);
+
+/* global mmApiDataDisplayBlock */
+
+
+
+
+
+
+function Edit({
+  attributes,
+  setAttributes
+}) {
+  const [error, setError] = (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.useState)(null);
+  const [record, setRecord] = (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.useState)(null);
+  const [isLoaded, setIsLoaded] = (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.useState)(false);
+  const htmlStructure = [(0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wordpress_block_editor__WEBPACK_IMPORTED_MODULE_3__.InspectorControls, {
+    key: "mm-api-data-display-block-inspector-controls"
+  }, (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wordpress_components__WEBPACK_IMPORTED_MODULE_1__.PanelBody, {
+    title: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_4__.__)('Manage Table columns', 'manisha-makhija'),
+    key: "mm-api-data-display-block-panel-body",
+    initialOpen: true
+  }, (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wordpress_components__WEBPACK_IMPORTED_MODULE_1__.ToggleControl, {
+    label: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_4__.__)('Show ID', 'manisha-makhija'),
+    onChange: value => {
+      setAttributes({
+        id: value
+      });
+    },
+    checked: attributes.id
+  }), (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wordpress_components__WEBPACK_IMPORTED_MODULE_1__.ToggleControl, {
+    label: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_4__.__)('Show First Name', 'manisha-makhija'),
+    onChange: value => {
+      setAttributes({
+        first_name: value
+      });
+    },
+    checked: attributes.first_name
+  }), (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wordpress_components__WEBPACK_IMPORTED_MODULE_1__.ToggleControl, {
+    label: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_4__.__)('Show Last Name', 'manisha-makhija'),
+    onChange: value => {
+      setAttributes({
+        last_name: value
+      });
+    },
+    checked: attributes.last_name
+  }), (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wordpress_components__WEBPACK_IMPORTED_MODULE_1__.ToggleControl, {
+    label: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_4__.__)('Show Email', 'manisha-makhija'),
+    onChange: value => {
+      setAttributes({
+        email: value
+      });
+    },
+    checked: attributes.email
+  }), (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wordpress_components__WEBPACK_IMPORTED_MODULE_1__.ToggleControl, {
+    label: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_4__.__)('Show Date', 'manisha-makhija'),
+    onChange: value => {
+      setAttributes({
+        date: value
+      });
+    },
+    checked: attributes.date
+  })))];
+  (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.useEffect)(() => {
+    _wordpress_api_fetch__WEBPACK_IMPORTED_MODULE_2___default()({
+      path: mmApiDataDisplayBlock.custom_endpoint
+    }).then(result => {
+      setIsLoaded(true);
+      setRecord(result);
+    }, err => {
+      setIsLoaded(true);
+      setError(err);
+    });
+  }, []);
+  if (error) {
+    return (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("p", null, (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_4__.sprintf)( /* Translators: %s - error message. */
+    (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_4__.__)('ERROR: %s', 'manisha-makhija'), error.message), ' ');
+  } else if (!isLoaded) {
+    return (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("p", null, (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_4__.__)('Loading response…', 'manisha-makhija'), " ");
+  } else if (record) {
+    const dataHeaders = record.data.headers;
+    const dataRows = record.data.rows;
+    if (dataHeaders.length < 0 || Object.keys(dataRows).length < 0) {
+      return (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("p", null, (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_4__.__)('No data found in API', 'manisha-makhija'));
+    }
+    htmlStructure.push([(0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("div", {
+      key: "manisha-makhija-table-data-wrap",
+      className: "manisha-makhija-table-data-wrap"
+    }, (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("table", {
+      border: "1",
+      style: {
+        width: '100%',
+        textAlign: 'center'
+      }
+    }, record.title && (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("tr", null, (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("td", {
+      colSpan: "5",
+      style: {
+        fontSize: 'xx-large',
+        padding: '25px'
+      }
+    }, record.title)), (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("tr", null, (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(TableHeader, {
+      header: dataHeaders,
+      attributes: attributes
+    })), (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(TableRows, {
+      rows: dataRows,
+      attributes: attributes
+    })))]);
+  }
+  return htmlStructure;
+}
+function TableHeader({
+  header,
+  attributes
+}) {
+  const headerStructure = new Array();
+  for (let i = 0; i < header.length; i++) {
+    const sanitizeHeaderValue = header[i].replace(' ', '_');
+    const headerKey = sanitizeHeaderValue.toLowerCase();
+    headerStructure.push([(0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, attributes[headerKey] && (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("th", null, header[i]))]);
+  }
+  return headerStructure;
+}
+function TableRows({
+  rows,
+  attributes
+}) {
+  const recordRows = new Array();
+  Object.values(rows).forEach(val => {
+    const date = new Date(val.date * 1000);
+    recordRows.push([(0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("tr", {
+      key: "mm-api-data-display-table-rows"
+    }, attributes.id && (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("td", null, val.id), attributes.first_name && (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("td", null, val.fname), attributes.last_name && (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("td", null, val.lname), attributes.email && (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("td", null, val.email), attributes.date && (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("td", null, date.toLocaleDateString('en-US')))]);
+  });
+  return recordRows;
+}
+
+/***/ }),
+
+/***/ "@wordpress/api-fetch":
+/*!**********************************!*\
+  !*** external ["wp","apiFetch"] ***!
+  \**********************************/
+/***/ ((module) => {
+
+module.exports = window["wp"]["apiFetch"];
+
+/***/ }),
+
+/***/ "@wordpress/block-editor":
+/*!*************************************!*\
+  !*** external ["wp","blockEditor"] ***!
+  \*************************************/
+/***/ ((module) => {
+
+module.exports = window["wp"]["blockEditor"];
+
+/***/ }),
+
+/***/ "@wordpress/blocks":
+/*!********************************!*\
+  !*** external ["wp","blocks"] ***!
+  \********************************/
+/***/ ((module) => {
+
+module.exports = window["wp"]["blocks"];
+
+/***/ }),
+
+/***/ "@wordpress/components":
+/*!************************************!*\
+  !*** external ["wp","components"] ***!
+  \************************************/
+/***/ ((module) => {
+
+module.exports = window["wp"]["components"];
+
+/***/ }),
+
+/***/ "@wordpress/element":
+/*!*********************************!*\
+  !*** external ["wp","element"] ***!
+  \*********************************/
+/***/ ((module) => {
+
+module.exports = window["wp"]["element"];
+
+/***/ }),
+
+/***/ "@wordpress/i18n":
+/*!******************************!*\
+  !*** external ["wp","i18n"] ***!
+  \******************************/
+/***/ ((module) => {
+
+module.exports = window["wp"]["i18n"];
+
+/***/ }),
+
+/***/ "./src/Block/block.json":
+/*!******************************!*\
+  !*** ./src/Block/block.json ***!
+  \******************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"$schema":"https://schemas.wp.org/trunk/block.json","apiVersion":1,"name":"manisha-makhija/api-data-display","title":"Manisha Makhija ","category":"widgets","icon":"editor-table","description":"Insert the API data response in a table form.","attributes":{"id":{"type":"boolean","default":true},"first_name":{"type":"boolean","default":true},"last_name":{"type":"boolean","default":true},"email":{"type":"boolean","default":true},"date":{"type":"boolean","default":true}},"keywords":["API"],"textdomain":"manisha-makhija","editorScript":"file:./index.js"}');
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+/*!****************************!*\
+  !*** ./src/Block/index.js ***!
+  \****************************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _wordpress_blocks__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @wordpress/blocks */ "@wordpress/blocks");
+/* harmony import */ var _wordpress_blocks__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_wordpress_blocks__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _edit__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./edit */ "./src/Block/edit.js");
+/* harmony import */ var _block_json__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./block.json */ "./src/Block/block.json");
+
+
+
+(0,_wordpress_blocks__WEBPACK_IMPORTED_MODULE_0__.registerBlockType)(_block_json__WEBPACK_IMPORTED_MODULE_2__.name, {
+  /**
+   * Used to construct a preview for the block to be shown in the block inserter.
+   */
+  example: {
+    attributes: {
+      preview: true
+    }
+  },
+  /**
+   * @see ./edit.js
+   */
+  edit: _edit__WEBPACK_IMPORTED_MODULE_1__["default"],
+  save() {
+    return null;
+  }
+});
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=index.js.map
